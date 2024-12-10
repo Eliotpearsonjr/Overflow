7d651361a4fa4106dfbac500e34a8bf28f8d8f7d
Eliot Pearson Jr <eliotnumber2@gmail.com>
2024-12-09 23:51:58 -0500

    created file for aqua graph view

---
+++ Water Tracker/Aqua_Graph_View.swift
@@ -0,0 +1,18 @@
+//
+//  Aqua_Graph_View.swift
+//  Water Tracker
+//
+//  Created by Eliot Pearson Jr on 12/9/24.
+//
+
+import SwiftUI
+
+struct Aqua_Graph_View: View {
+    var body: some View {
+        Text("Aqua Graph goes here!")
+    }
+}
+
+#Preview {
+    Aqua_Graph_View()
+}
